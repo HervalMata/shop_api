48fc3463b7af2e4c4652f4fdb59e09a35af33a65
HervalMata <hervalmataamparo@gmail.com>
2020-07-29 21:05:08 -0300

    Metodo index para Categoria

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        $categories = Category::paginate(5);
+        return CategoryResource::collection($categories);
     }
 
     /**
