5fbe5f227b4e264dd74afe3a905fd7d8d0f47b82
HervalMata <hervalmataamparo@gmail.com>
2020-08-03 18:28:56 -0300

    Metodo index de Material

--- app/Http/Controllers/Api/MaterialController.php
+++ app/Http/Controllers/Api/MaterialController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\MaterialFilter;
+use App\Http\Resources\MaterialResource;
+use App\Models\Material;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MaterialController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @apiResource MaterialResource
+     * @apiModel Material
+     * @param Request $request
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $filter = app(MaterialFilter::class);
+        $query = Material::query();
+        $filterQuery = $query->filtered($filter);
+        $materials = $request->has('all') ? $filterQuery->get() : $filterQuery->paginate(5);
+        return MaterialResource::collection($materials);
     }
 
     /**
