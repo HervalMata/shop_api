bf09393645bba9322a76f3d52d7c73ce1b0ef32e
HervalMata <hervalmataamparo@gmail.com>
2020-08-03 19:41:58 -0300

    Metodo store de produtos com Material

--- app/Http/Controllers/Api/ProductMaterialController.php
+++ app/Http/Controllers/Api/ProductMaterialController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductMaterialRequest;
 use App\Http\Resources\ProductMaterialResource;
+use App\Models\Material;
 use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductMaterialController extends Controller
@@ -25,12 +28,18 @@ class ProductMaterialController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @apiResource ProductColorResource
+     * @apiModel Product
+     * @param ProductMaterialRequest $request
+     * @param Product $product
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(ProductMaterialRequest $request, Product $product)
     {
-        //
+        $changed = $product->materials()->sync($request->materials);
+        $materialsAttachedId = $changed['attached'];
+        $materials = Material::whereIn('id', $materialsAttachedId)->get();
+        return $materials->count() ? response()->json(new ProductMaterialResource($product), 201) : [];
     }
 
     /**
