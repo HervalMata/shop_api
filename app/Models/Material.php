b5060d2982458901306716e25a3edad3a3840f74
HervalMata <hervalmataamparo@gmail.com>
2020-08-03 17:58:57 -0300

    Model de Material

---
+++ app/Models/Material.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Mnabialek\LaravelEloquentFilter\Traits\Filterable;
+
+class Material extends Model
+{
+    use Filterable;
+
+    protected $fillable = ['material_name', 'slug'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($color) {
+            $color->slug = Str::slug($color->color_name);
+        });
+    }
+}
