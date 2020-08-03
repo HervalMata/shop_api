a53bbeb89704e4c7d48dfefa3dd12e900b89d9cf
HervalMata <hervalmataamparo@gmail.com>
2020-08-02 23:44:27 -0300

    Metodo index de Cores

--- app/Http/Controllers/Api/ColorController.php
+++ app/Http/Controllers/Api/ColorController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\ColorFilter;
+use App\Http\Resources\ColorResource;
+use App\Models\Color;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ColorController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $filter = app(ColorFilter::class);
+        $query = Color::query();
+        $query = $this->onlyTrashedIfRequested($request, $query);
+        $filterQuery = $query->filtered($filter);
+        $color = $request->has('all') ? $filterQuery->get() : $filterQuery->paginate(5);
+        return ColorResource::collection($color);
     }
 
     /**
@@ -60,5 +71,18 @@ class ColorController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * @param Request $request
+     * @param Builder $query
+     * @return Builder
+     */
+    private function onlyTrashedIfRequested(Request $request, Builder $query)
+    {
+        if ($request->get('trashed') == 1) {
+            $query = $query->onlyTrashed();
+        }
+        return $query;
     }
 }
