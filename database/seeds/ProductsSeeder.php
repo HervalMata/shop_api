11d42025187932c9434b6fc43bfed02aae608e2e
HervalMata <hervalmataamparo@gmail.com>
2020-08-03 00:49:20 -0300

    Refatorando seeder de produtos com Cores

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Color;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        factory(Product::class, 100)->create();
+        $colors = Color::all();
+        factory(Product::class, 100)
+            ->create()
+            ->each(function (Product $product) use ($colors) {
+                for ($i = 1; $i < 3; $i++) {
+                    $colorId = $colors->random()->id;
+                    $product->colors()->attach($colorId);
+                }
+            });
     }
 }
