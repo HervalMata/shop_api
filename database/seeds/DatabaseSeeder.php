532ad6d8238ad5f0544332e3a5511e98ebee2d81
HervalMata <hervalmataamparo@gmail.com>
2020-08-02 23:27:36 -0300

    Seeder de Cores

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(CategoriesSeeder::class);
         $this->call(ProductsSeeder::class);
+        $this->call(ColorsSeeder::class);
     }
 }
