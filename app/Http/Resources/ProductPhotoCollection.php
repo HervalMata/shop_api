f5f568b04cc82a591979e0b1e41bceb49a89a8a0
HervalMata <hervalmataamparo@gmail.com>
2020-08-03 23:18:13 -0300

    resource e collection de fotos dos produtos

---
+++ app/Http/Resources/ProductPhotoCollection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Product;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductPhotoCollection extends ResourceCollection
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * ProductPhotoCollection constructor.
+     * @param $resource
+     * @param Product $product
+     */
+    public function __construct($resource, Product $product)
+    {
+        parent::__construct($resource);
+        $this->product = $product;
+    }
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'product' => new ProductResource($this->product),
+            'photos' => $this->collection->map(function ($photo) {
+                return new ProductPhotoResource($photo, true);
+            })
+        ];
+    }
+}
